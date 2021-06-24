5b96b63702c721d37aafebd14ab37644d44ae004
Qasim Malik <81699451+qaxim-malik@users.noreply.github.com>
2021-06-24 21:41:04 +0500

    Updates

---
+++ precache-manifest.6c5da4a2c300208993a29cafc698617c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8af7e92f29daca1256dfb0b6a31112",
+    "url": "/react-search-github-users/index.html"
+  },
+  {
+    "revision": "effd54926aa5476840e4",
+    "url": "/react-search-github-users/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "d370c128126d140d056d",
+    "url": "/react-search-github-users/static/js/2.28efaf96.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/react-search-github-users/static/js/2.28efaf96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effd54926aa5476840e4",
+    "url": "/react-search-github-users/static/js/main.2426cd17.chunk.js"
+  },
+  {
+    "revision": "8f2791f78586de2c66c5",
+    "url": "/react-search-github-users/static/js/runtime-main.4e09cbe4.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/react-search-github-users/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/react-search-github-users/static/media/preloader.c114fda3.gif"
+  }
+]);
